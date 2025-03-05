bed624c812bb30ac55a1ca24016e77d7f5dd913e
Richard Flores <lcdo.richardflores@gmail.com>
2025-03-05 17:24:55 -0300

    portfolio, env contain schema

---
+++ src/constants/index.js
@@ -0,0 +1,252 @@
+import {
+	mobile,
+	backend,
+	creator,
+	web,
+	javascript,
+	typescript,
+	html,
+	css,
+	reactjs,
+	redux,
+	tailwind,
+	nodejs,
+	mongodb,
+	git,
+	figma,
+	docker,
+	meta,
+	starbucks,
+	tesla,
+	shopify,
+	carrent,
+	jobit,
+	tripguide,
+	threejs,
+} from "../assets";
+
+export const navLinks = [
+	{
+		id: "about",
+		title: "About",
+	},
+	{
+		id: "work",
+		title: "Work",
+	},
+	{
+		id: "contact",
+		title: "Contact",
+	},
+];
+
+const services = [
+	{
+		title: "Web Developer",
+		icon: web,
+	},
+	{
+		title: "React Developer",
+		icon: mobile,
+	},
+	{
+		title: "Backend Developer",
+		icon: backend,
+	},
+	{
+		title: "Content Creator",
+		icon: creator,
+	},
+];
+
+const technologies = [
+	{
+		name: "HTML 5",
+		icon: html,
+	},
+	{
+		name: "CSS 3",
+		icon: css,
+	},
+	{
+		name: "JavaScript",
+		icon: javascript,
+	},
+	{
+		name: "TypeScript",
+		icon: typescript,
+	},
+	{
+		name: "React JS",
+		icon: reactjs,
+	},
+	{
+		name: "Redux Toolkit",
+		icon: redux,
+	},
+	{
+		name: "Tailwind CSS",
+		icon: tailwind,
+	},
+	{
+		name: "Node JS",
+		icon: nodejs,
+	},
+	{
+		name: "MongoDB",
+		icon: mongodb,
+	},
+	{
+		name: "Three JS",
+		icon: threejs,
+	},
+	{
+		name: "git",
+		icon: git,
+	},
+	{
+		name: "figma",
+		icon: figma,
+	},
+	{
+		name: "docker",
+		icon: docker,
+	},
+];
+
+const experiences = [
+	{
+		title: "Full Stack Developer",
+		company_name: "Upwork",
+		icon: meta,
+		iconBg: "#383E56",
+		date: "Jan 2023 - Present",
+		points: [
+			"Desarrollando y manteniendo aplicaciones web utilizando MERN stack y WordPress.",
+			"Trabajando con clientes internacionales para diseñar y optimizar embudos de venta en GoHighLevel.",
+			"Implementando estrategias de SEO y optimización para mejorar la conversión en sitios web.",
+			"Colaborando en proyectos de e-commerce y plataformas personalizadas con alta escalabilidad.",
+		],
+	},
+	{
+		title: "Web Developer & SEO Specialist",
+		company_name: "Socialties",
+		icon: meta,
+		iconBg: "#E6DEDD",
+		date: "Sep 2022 - Dec 2022",
+		points: [
+			"Diseño y desarrollo de sitios web personalizados utilizando WordPress y Elementor.",
+			"Optimización de contenido para mejorar el posicionamiento SEO y aumentar la conversión.",
+			"Integración de herramientas de automatización y marketing digital para maximizar el rendimiento de las campañas.",
+			"Colaboración con el equipo de marketing para alinear estrategias digitales con los objetivos de negocio.",
+		],
+	},
+	{
+		title: "Gerente Comercial",
+		company_name: "Científica Inca",
+		icon: meta,
+		iconBg: "#383E56",
+		date: "Jan 2018 - Aug 2022",
+		points: [
+			"Gestión de ventas y relaciones comerciales en el sector de reactivos, insumos y equipos para laboratorio clínico.",
+			"Análisis de mercado y estrategias de expansión para aumentar la cuota de mercado.",
+			"Manejo de inventarios y planificación logística para garantizar disponibilidad de productos clave.",
+			"Negociación con proveedores y clientes para optimizar costos y mejorar márgenes de ganancia.",
+		],
+	},
+];
+
+const testimonials = [
+	{
+		testimonial:
+			"I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+		name: "Sara Lee",
+		designation: "CFO",
+		company: "Acme Co",
+		image: "https://randomuser.me/api/portraits/women/4.jpg",
+	},
+	{
+		testimonial:
+			"I've never met a web developer who truly cares about their clients' success like Rick does.",
+		name: "Chris Brown",
+		designation: "COO",
+		company: "DEF Corp",
+		image: "https://randomuser.me/api/portraits/men/5.jpg",
+	},
+	{
+		testimonial:
+			"After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+		name: "Lisa Wang",
+		designation: "CTO",
+		company: "456 Enterprises",
+		image: "https://randomuser.me/api/portraits/women/6.jpg",
+	},
+];
+
+const projects = [
+	{
+		name: "Car Rent",
+		description:
+			"Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+		tags: [
+			{
+				name: "react",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "mongodb",
+				color: "green-text-gradient",
+			},
+			{
+				name: "tailwind",
+				color: "pink-text-gradient",
+			},
+		],
+		image: carrent,
+		source_code_link: "https://github.com/",
+	},
+	{
+		name: "Job IT",
+		description:
+			"Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+		tags: [
+			{
+				name: "react",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "restapi",
+				color: "green-text-gradient",
+			},
+			{
+				name: "scss",
+				color: "pink-text-gradient",
+			},
+		],
+		image: jobit,
+		source_code_link: "https://github.com/",
+	},
+	{
+		name: "Trip Guide",
+		description:
+			"A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+		tags: [
+			{
+				name: "nextjs",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "supabase",
+				color: "green-text-gradient",
+			},
+			{
+				name: "css",
+				color: "pink-text-gradient",
+			},
+		],
+		image: tripguide,
+		source_code_link: "https://github.com/",
+	},
+];
+
+export { services, technologies, experiences, testimonials, projects };
