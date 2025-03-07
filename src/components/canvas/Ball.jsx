627852ddf3d335cd489ba38769f90ac907d5ffcd
Richard <lcdo.richardflores@gmail.com>
2025-03-07 13:28:52 -0300

    technologies in blue

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -7,32 +7,35 @@ import {
 	Preload,
 	useTexture,
 } from "@react-three/drei";
+import * as THREE from "three";
 
 import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
-	const [decal] = useTexture([props.imgUrl]);
+	const [decal] = useTexture([props.imgUrl], (loader) => {
+		loader.colorSpace = THREE.SRGBColorSpace;
+	});
 
 	return (
 		<Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-			<ambientLight intensity={1} />
-			<directionalLight position={[0, 0, 0.05]} />
+			<ambientLight intensity={0.75} />
+			<directionalLight position={[0, 0, 0.5]} intensity={2} />
 			<mesh castShadow receiveShadow scale={2.75}>
 				<icosahedronGeometry args={[1, 1]} />
 				<meshStandardMaterial
-					color="#d4d4d4"
+					color="#090f66"
 					polygonOffset
 					polygonOffsetFactor={-5}
-					flatShading
+					flatShading={true}
+					metalness={0.2}
+					roughness={0.4}
+					alphaTest={0.5}
 				/>
 				<Decal
 					position={[0, 0, 1]}
 					rotation={[2 * Math.PI, 0, 6.25]}
-					scale={1}
+					scale={1.05}
 					map={decal}
-					depthTest={false}
-					depthWrite={true}
-					flatShading={false}
 				/>
 			</mesh>
 		</Float>
@@ -44,7 +47,11 @@ const BallCanvas = ({ icon }) => {
 		<Canvas
 			frameloop="demand"
 			dpr={[1, 2]}
-			gl={{ preserveDrawingBuffer: true }}>
+			gl={{
+				preserveDrawingBuffer: true,
+				outputColorSpace: "srgb", // Forzar espacio de color sRGB
+				toneMapping: THREE.NoToneMapping, // Desactivar tone mapping global
+			}}>
 			<Suspense fallback={<CanvasLoader />}>
 				<OrbitControls enableZoom={false} />
 				<Ball imgUrl={icon} />
